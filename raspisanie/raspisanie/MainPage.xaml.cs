f7700af664cded64a043283c62eb90a79bc6c1cf
opilane <opilane@HP-CZC6297B8P>
2020-10-12 13:48:20 +0300

    sd

--- raspisanie/raspisanie/MainPage.xaml.cs
+++ raspisanie/raspisanie/MainPage.xaml.cs
@@ -12,6 +12,8 @@ namespace raspisanie
     {
         //BoxView box;
         Label  nomer1, nomer2, nomer3, nomer4, nomer5, nomer6, nomer7, nomer8, pon, vtor, sre, chet, pjat, uroki1, uroki2, uroki3, uroki4, uroki5, uroki6, uroki7, uroki8, uroki9, uroki10, uroki11, uroki12, uroki13, uroki14;
+
+
         //Frame frm, frm2;
         public MainPage()
         {
@@ -52,41 +54,145 @@ namespace raspisanie
             abs.Children.Add(chet, 0, 4); 
             pjat = new Label { BackgroundColor = Color.White, Text = "\n \n Пятница" };
             abs.Children.Add(pjat, 0, 5);
-            uroki1 = new Label { BackgroundColor = Color.White, Text = "Keel ja \n Kirjandus" };
+            uroki1 = new Label { BackgroundColor = Color.LightGreen, Text = "Keel ja \n Kirjandus" };
             abs.Children.Add(uroki1, 1, 1);
             Grid.SetColumnSpan(uroki1, 2);
-            uroki2 = new Label { BackgroundColor = Color.White, Text = "Võrgud ja Seadm." };
-            abs.Children.Add(uroki2, 5, 1);
+            uroki2 = new Label { BackgroundColor = Color.Beige, Text = "Võrgud ja Seadm." };
+            abs.Children.Add(uroki2, 3, 1);
             Grid.SetColumnSpan(uroki2, 3);
-            uroki3 = new Label { BackgroundColor = Color.White, Text = "Mob. Rak." };
-            abs.Children.Add(uroki3, 8, 1);
+            uroki3 = new Label { BackgroundColor = Color.DeepSkyBlue, Text = "Mob. Rak." };
+            abs.Children.Add(uroki3, 7, 1);
             Grid.SetColumnSpan(uroki3, 3);
-            uroki4 = new Label { BackgroundColor = Color.White, Text = "Transp.log.hut." };
-            abs.Children.Add(uroki4, 3, 2);
+            uroki4 = new Label { BackgroundColor = Color.LightSeaGreen, Text = "Transp.log.hut." };
+            abs.Children.Add(uroki4, 1, 2);
             Grid.SetColumnSpan(uroki4, 3);
-            uroki5 = new Label { BackgroundColor = Color.White, Text = "Inglise W.hald" };
-            abs.Children.Add(uroki5, 7, 2);
+            uroki5 = new Label { BackgroundColor = Color.Gray, Text = "Inglise W.hald" };
+            abs.Children.Add(uroki5, 5, 2);
             Grid.SetColumnSpan(uroki5, 2);
-            uroki6 = new Label { BackgroundColor = Color.White, Text = "Eesti keel \n teise kellena" };
-            abs.Children.Add(uroki6, 9, 2);
+            uroki6 = new Label { BackgroundColor = Color.Pink, Text = "Eesti keel \n teise kellena" };
+            abs.Children.Add(uroki6, 7, 2);
             Grid.SetColumnSpan(uroki6, 2);
-            uroki7 = new Label { BackgroundColor = Color.White, Text = "W.paig.sead." };
-            abs.Children.Add(uroki7, 3, 3);
+            uroki7 = new Label { BackgroundColor = Color.Violet, Text = "W.paig.sead." };
+            abs.Children.Add(uroki7, 1, 3);
             Grid.SetColumnSpan(uroki7, 3);
-            uroki8 = new Label { BackgroundColor = Color.White, Text = "Transp.log.hut." };
-            abs.Children.Add(uroki8, 6, 3);
+            uroki8 = new Label { BackgroundColor = Color.LightSeaGreen, Text = "Transp.log.hut." };
+            abs.Children.Add(uroki8, 4, 3);
             Grid.SetColumnSpan(uroki8, 5);
-            uroki9 = new Label { BackgroundColor = Color.White, Text = "Keemia \n Biologia" };
-            abs.Children.Add(uroki9, 11, 3);
-            uroki10 = new Label { BackgroundColor = Color.White, Text = "W.paig.sead." };
-            abs.Children.Add(uroki10, 3, 4);
+            uroki9 = new Label { BackgroundColor = Color.DarkViolet, Text = "Keemia \n Biologia" };
+            abs.Children.Add(uroki9, 9, 3);
+            uroki10 = new Label { BackgroundColor = Color.Violet, Text = "W.paig.sead." };
+            abs.Children.Add(uroki10, 1, 4);
             Grid.SetColumnSpan(uroki10, 3);
-            uroki11 = new Label { BackgroundColor = Color.White, Text = "Võrgud ja Seadm." };
-            abs.Children.Add(uroki11, 7, 4);
-            uroki12 = new Label { BackgroundColor = Color.White, Text = "Inglise W.Hald" };
-            abs.Children.Add(uroki12, 9, 4);
+            uroki11 = new Label { BackgroundColor = Color.Beige, Text = "Võrgud ja Seadm." };
+            abs.Children.Add(uroki11, 5, 4);
+            Grid.SetColumnSpan(uroki11, 2);
+            uroki12 = new Label { BackgroundColor = Color.Gray, Text = "Inglise W.Hald" };
+            abs.Children.Add(uroki12, 7, 4);
             Grid.SetColumnSpan(uroki12, 2);
+            uroki13 = new Label { BackgroundColor = Color.DarkViolet, Text = "Keemia, bioloogia eesti keeles" };
+            abs.Children.Add(uroki13, 1, 5);
+            uroki14 = new Label { BackgroundColor = Color.DeepSkyBlue, Text = "Mob.rak" };
+            abs.Children.Add(uroki14, 3, 5);
+            Grid.SetColumnSpan(uroki14, 3);
             Content = abs;
+            var tap1 = new TapGestureRecognizer();
+            tap1.Tapped += async (s, e) =>
+            {
+                uroki1 = (Label)s;
+                await DisplayAlert("Доп. инфо", "Кабинет B221 \n Mihhailova Ljudmilla", "закрыть");
+            };
+            uroki1.GestureRecognizers.Add(tap1);
+            var tap2 = new TapGestureRecognizer();
+            tap2.Tapped += async (s, e) =>
+            {
+                uroki2 = (Label)s;
+                await DisplayAlert("Доп. инфо", "Кабинет A123 \n Agapov Mikhail", "закрыть");
+            };
+            uroki2.GestureRecognizers.Add(tap2);
+            var tap3 = new TapGestureRecognizer();
+            tap3.Tapped += async (s, e) =>
+            {
+                uroki3 = (Label)s;
+                await DisplayAlert("Доп. инфо", "Кабинет E07 \n Oleinik Marina", "закрыть");
+            };
+            uroki3.GestureRecognizers.Add(tap3);
+            var tap4 = new TapGestureRecognizer();
+            tap4.Tapped += async (s, e) =>
+            {
+                uroki4 = (Label)s;
+                await DisplayAlert("Доп. инфо", "Кабинет B002 \n Krull Jaanus", "закрыть");
+            };
+            uroki4.GestureRecognizers.Add(tap4);
+            var tap5 = new TapGestureRecognizer();
+            tap5.Tapped += async (s, e) =>
+            {
+                uroki5 = (Label)s;
+                await DisplayAlert("Доп. инфо", "Кабинет B242 \n Voronetskaja Julia", "закрыть");
+            };
+            uroki5.GestureRecognizers.Add(tap5);
+            var tap6 = new TapGestureRecognizer();
+            tap6.Tapped += async (s, e) =>
+            {
+                uroki6 = (Label)s;
+                await DisplayAlert("Доп. инфо", "Кабинет B236 \n Laaneväli-Toots Alina", "закрыть");
+            };
+            uroki6.GestureRecognizers.Add(tap6);
+            var tap7 = new TapGestureRecognizer();
+            tap7.Tapped += async (s, e) =>
+            {
+                uroki7 = (Label)s;
+                await DisplayAlert("Доп. инфо", "Кабинет A243 \n Shkarbanova Lury", "закрыть");
+            };
+            uroki7.GestureRecognizers.Add(tap7);
+            var tap8 = new TapGestureRecognizer();
+            tap8.Tapped += async (s, e) =>
+            {
+                uroki8 = (Label)s;
+                await DisplayAlert("Доп. инфо", "Кабинет B002 \n Krull Jaanus", "закрыть");
+            };
+            uroki8.GestureRecognizers.Add(tap8);
+            var tap9 = new TapGestureRecognizer();
+            tap9.Tapped += async (s, e) =>
+            {
+                uroki9 = (Label)s;
+                await DisplayAlert("Доп. инфо", "Кабинет B144 \n Pesetskaja Svetlana", "закрыть");
+            };
+            uroki9.GestureRecognizers.Add(tap9);
+            var tap10 = new TapGestureRecognizer();
+            tap10.Tapped += async (s, e) =>
+            {
+                uroki10 = (Label)s;
+                await DisplayAlert("Доп. инфо", "Кабинет a243 \n Shkarbanova Lury", "закрыть");
+            };
+            uroki10.GestureRecognizers.Add(tap10);
+            var tap11 = new TapGestureRecognizer();
+            tap11.Tapped += async (s, e) =>
+            {
+                uroki11 = (Label)s;
+                await DisplayAlert("Доп. инфо", "Кабинет A123 \n Agapov Mikhail", "закрыть");
+            };
+            uroki11.GestureRecognizers.Add(tap11);
+            var tap12 = new TapGestureRecognizer();
+            tap12.Tapped += async (s, e) =>
+            {
+                uroki12 = (Label)s;
+                await DisplayAlert("Доп. инфо", "Кабинет B242 \n Voronetskaja Julia", "закрыть");
+            };
+            uroki12.GestureRecognizers.Add(tap12);
+            var tap13 = new TapGestureRecognizer();
+            tap13.Tapped += async (s, e) =>
+            {
+                uroki13 = (Label)s;
+                await DisplayAlert("Доп. инфо", "Кабинет B144 \n Pesetskaja Svetlana", "закрыть");
+            };
+            uroki13.GestureRecognizers.Add(tap13);
+            var tap14 = new TapGestureRecognizer();
+            tap14.Tapped += async (s, e) =>
+            {
+                uroki14 = (Label)s;
+                await DisplayAlert("Доп. инфо", "Кабинет E007 \n Oleinik Marina", "закрыть");
+            };
+            uroki14.GestureRecognizers.Add(tap14);
         }
     }
 }
